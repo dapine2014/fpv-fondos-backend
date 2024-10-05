143ffd05a3ca82540218ad1cb831b1d93226220b
alex <dapine@gmail.com>
2024-10-05 17:55:35 -0500

    Initial commit

---
+++ src/main/java/fondos/fpvfondosbackend/FpvFondosBackendApplication.java
@@ -0,0 +1,13 @@
+package fondos.fpvfondosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpvFondosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpvFondosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fondos/fpvfondosbackend/FpvFondosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fondos.fpvfondosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpvFondosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
